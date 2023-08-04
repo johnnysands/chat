c755e4aa0fc8369752265815e917f381961d83e2
Johnny Sands <johnnysands@users.noreply.github.com>
2023-08-04 16:18:28 +0300

    add basic session support

--- app.py
+++ app.py
@@ -1,23 +1,38 @@
-from flask import Flask, render_template, request, url_for
-import bandolier
+from flask import Flask, render_template, request, url_for, session
+from bandolier import Bandolier
+from flask_session import Session  # you need to install this module
 
-bandolier = bandolier.Bandolier()
-bandolier.add_system_message("You are a helpful assistant.")
 
 app = Flask(__name__)
+
+# Flask session configuration
+app.config["SESSION_TYPE"] = "filesystem"
+app.config["SECRET_KEY"] = "889cee86add812714b96fa96c57ea31024157ad21ced3bc56292c61d"
+Session(app)
 
 
 @app.route("/")
 def chat():
-    return render_template("chat.html")
+    if "messages" not in session:
+        session["messages"] = []
+    return render_template("chat.html", messages=session["messages"])
 
 
 @app.route("/send-message", methods=["POST"])
 def send_message():
+    # initialize bandolier with stored state
+    bandolier = Bandolier()
+    bandolier.messages = session["messages"]
+
+    # process the incoming message
     message = request.form.get("message")
     bandolier.add_user_message(message)
     response = bandolier.run()
-    return f'<div class="chat-message server-message"><span class="username">{response.role}</span>{response.content}</div>'
+
+    # send the response
+    session["messages"] = bandolier.messages
+    session.modified = True
+    return f'<div class="chat-message server-message"><span class="username">{response.role}:</span> {response.content}</div>'
 
 
 if __name__ == "__main__":
