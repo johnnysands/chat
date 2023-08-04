1d89010b59914c8c0d855899ae448f02e123cbf8
Johnny Sands <johnnysands@users.noreply.github.com>
2023-08-04 17:46:16 +0300

    add functions and fix some bugs related to functions

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from bandolier import Bandolier
+from bandolier import Bandolier, annotate_arguments, annotate_description
 from flask import Flask, render_template, request, url_for, session
 from flask_session import Session
 
@@ -17,7 +17,16 @@ Session(app)
 def chat():
     if "messages" not in session:
         session["messages"] = []
-    return render_template("chat.html", messages=session["messages"])
+
+    messages = []
+    for m in session["messages"]:
+        # filter out function calls
+        if m.content is None:
+            continue
+        # filter out all other roles
+        if m["role"] == "assistant" or m["role"] == "user":
+            messages.append(m)
+    return render_template("chat.html", messages=messages)
 
 
 @app.route("/new-chat", methods=["POST"])
@@ -33,6 +42,7 @@ def send_message():
     # initialize bandolier with stored state
     bandolier = Bandolier()
     bandolier.messages = session["messages"]
+    bandolier.add_function(get_weather)
 
     # process the incoming message
     message = request.form.get("message")
@@ -43,6 +53,30 @@ def send_message():
     session["messages"] = bandolier.messages
     session.modified = True
     return f'<div class="chat-message server-message"><span class="username">{response.role}:</span> {response.content}</div>'
+
+
+@annotate_arguments(
+    {
+        "location": {
+            "type": "string",
+            "description": "The city and state, e.g. San Francisco, CA.",
+        },
+        "unit": {
+            "type": "string",
+            "description": "The unit to return the temperature in, e.g. F or C.",
+            "default": "F",
+        },
+    }
+)
+@annotate_description("Get the weather for a location.")
+def get_weather(location, unit="F"):
+    return {"temperature": 72, unit: unit, "conditions": ["sunny", "windy"]}
+
+
+def load_bandolier():
+    bandolier = Bandolier()
+    bandolier.add_function(get_weather)
+    return bandolier
 
 
 if __name__ == "__main__":
